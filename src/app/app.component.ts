4cfed9ab522dfb4f0563f7a8d3049fc02669219d
uditha1994 <uditha@programmer.net>
2024-02-11 20:14:04 +0530

    bootstrap and tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MainHeaderComponent } from './core/main-header/main-header.component';
+import { MainFooterComponent } from './core/main-footer/main-footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MainHeaderComponent, MainFooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
