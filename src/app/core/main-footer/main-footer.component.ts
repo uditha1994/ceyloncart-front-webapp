cc2dac0a836db5ad11f35193763f11173b6ddea1
uditha1994 <uditha@programmer.net>
2024-02-12 22:20:24 +0530

    footer creat

--- src/app/core/main-footer/main-footer.component.ts
+++ src/app/core/main-footer/main-footer.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-main-footer',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './main-footer.component.html',
   styleUrl: './main-footer.component.scss'
 })
 export class MainFooterComponent {
-
+  
 }
